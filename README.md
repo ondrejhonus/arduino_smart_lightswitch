f79252a107b3f338c8472bb735cebc3458f7a37b
Ondrej Honus - Laptop <ondrejhonus@proton.me>
2024-11-01 19:52:08 +0100

    servo broken

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nevermind, my servo is broken, so I'll get back to this once i get a new one lol
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,27 @@
-// letsarduino.com
-// [Project 11] - 2 Servos Using a Joystick 
-//  (thumbstick) + Arduino
-
 #include <Servo.h>  
 #include <Arduino.h>
 
-#define ServoXPin  9   
-#define ServoYPin   10   
-#define HorizontalPotPin  A1
-#define VerticalPotPin  A1 
-#define ServoMin  0  
-#define ServoMax  180
+#define POT_PIN  A0
+#define SERVO_PIN 9
+#define SERVO_MIN  0  
+#define SERVO_MAX  180
 
-Servo XServo;  
-Servo YServo;    
+Servo servo;  
 
-int XPos;    
-int YPos;    
-
-int joyX = A0;
-int joyY = A1;
-int JoyXVal;
-int JoyYVal;
+int pot_val;
+int servo_val;
 
 void setup()   
 {
-  XServo.attach(ServoXPin);   
-  YServo.attach(ServoYPin);         
+  servo.attach(SERVO_PIN);
 }
 
 void loop()  
 {
-  JoyXVal = analogRead(joyX);
-  JoyYVal = analogRead(joyY);
+  pot_val = analogRead(POT_PIN);
 
-  XPos  = map(JoyXVal, 0, 1023, ServoMin , ServoMax);
-  YPos  = map(JoyYVal, 1023, 0, ServoMin , ServoMax);
+  servo_val = map(pot_val, 0, 1023, SERVO_MIN , SERVO_MAX);
   
-  XServo.write(XPos);
-  YServo.write(YPos);
-  delay(20);
+  servo.write(servo_val);
+  delay(100);
 }
